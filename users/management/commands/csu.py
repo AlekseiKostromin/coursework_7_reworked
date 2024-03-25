c811e72b9ab6aaddd1d23b8187a9686a05e5b042
AlekseiKostromin <cs777@yandex.ru>
2024-03-25 10:53:56 +0500

    Add Dockerfile, dockercompose.yaml

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -5,12 +5,12 @@ from users.models import User
 class Command(BaseCommand):
     def handle(self, *args, **options):
         user = User.objects.create(
-            email='admin@csu',
-            first_name='Admin',
-            last_name='Adminov',
+            email='cs777@yandex.ru',
+            first_name='Aleksei',
+            last_name='Ko',
             is_staff=True,
             is_superuser=True,
             is_active=True,
         )
-        user.set_password('admin')
+        user.set_password('12345')
         user.save()
